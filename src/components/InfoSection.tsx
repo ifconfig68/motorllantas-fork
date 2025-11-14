5b1e0e5630ee3da4784525f34b186e2025a5f227
0p3r4d0r <anderson.valenzuela@udea.edu.co>
2025-11-14 12:46:50 -0500

    algo roto

--- src/components/InfoSection.tsx
+++ src/components/InfoSection.tsx
@@ -1,6 +1,6 @@
 //import React from 'react';
 import './InfoSection.css';
-import photocar from '../assets/img/photocar.JPG';
+import photocar from '../assets/img/photocar.jpg';
 import { Link } from 'react-router-dom';
 
 function InfoSection() {
