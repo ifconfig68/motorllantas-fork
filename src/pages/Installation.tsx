e4c42cd0d065897f8b40501ddfe635e591206e49
0p3r4d0r <anderson.valenzuela@udea.edu.co>
2025-11-14 13:11:06 -0500

    algo roto

--- src/pages/Installation.tsx
+++ src/pages/Installation.tsx
@@ -1,8 +1,8 @@
       import React from 'react';
       import './Installation.css';
-      
+      // @ts-ignore
       import installation1 from '../assets/img/instalacion1.JPG';
-      
+      // @ts-ignore
       import installation2 from '../assets/img/instalacion2.JPG';
       
       const Installation: React.FC = () => {
