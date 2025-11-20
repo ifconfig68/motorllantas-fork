c0ee054189605475f24ecd07ccfbc70ad9e88d82
0p3r4d0r <anderson.valenzuela@udea.edu.co>
2025-11-20 18:45:47 -0500

    ip meli

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -70,7 +70,7 @@ const Checkout = () => {
 
   const handlePay = async () => {
     try {
-      const response = await fetch('http://localhost:3000/api/payments/create_preference', {
+      const response = await fetch('http://3.90.219.227:3000/api/payments/create_preference', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
